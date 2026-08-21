f8d5760c5d1c3725db57fd364ca0348381b356ba
Evan <128929890+Evaxle@users.noreply.github.com>
2026-08-21 19:13:57 -0400

    Use the exported Vulnus executable directory for maps

--- scripts/Global.cs
+++ scripts/Global.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class Global : Node
 {
-	public static string MapPath = ProjectSettings.GlobalizePath("res://maps");
+	public static string MapPath = OS.HasFeature("editor") ? ProjectSettings.GlobalizePath("res://maps") : OS.GetExecutablePath().GetBaseDir().PlusFile("maps");
 
 	public static Global Instance;
 	public static Discord.DiscordW Discord;
